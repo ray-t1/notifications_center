f6069fb83b5f56499b6afe6be3db4286dc1bc38c
ramon <rtgg95@gmail.com>
2020-01-16 21:36:56 -0400

    Screenshot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { Ng2ScreenshotModule } from 'ng2-screenshot';
 
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { HeaderComponent} from  './header/header.component';
-import { BodyComponent } from './body/body.component'
+import { BodyComponent } from './body/body.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzCardModule } from 'ng-zorro-antd/card';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule,CommonModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule, CommonModule, Ng2ScreenshotModule, NzIconModule,
+    NzButtonModule,
+    NzSwitchModule,
+    NzCardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
